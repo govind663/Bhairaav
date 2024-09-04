8c1c3adfbfc774467e6522534ee92f2c39124c75
govind663 <whitdevil06@gmail.com>
2024-09-04 18:12:18 +0530

    Bhairaav | Version - 0.41

--- app/Http/Controllers/frontend/WhoWeAreController.php
+++ app/Http/Controllers/frontend/WhoWeAreController.php
@@ -3,11 +3,43 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Journey;
+use App\Models\Member;
+use App\Models\TheProgressDetail;
+use App\Models\Legacy;
+use App\Models\Strength;
+use App\Models\OurLogo;
 use Illuminate\Http\Request;
 
 class WhoWeAreController extends Controller
 {
     public function whoWeAre(Request $request){
-        return view("frontend.about.who-we-are");
+
+        // === Fetch The Journey
+        $journeys = Journey::orderBy("id","desc")->whereNull('deleted_at')->first();
+
+        // === Fetch The Members
+        $members = Member::orderBy("id","desc")->whereNull('deleted_at')->get();
+
+        // === Fetch The Progress
+        $progressDetails = TheProgressDetail::orderBy("id","desc")->whereNull('deleted_at')->get();
+
+        // === Fetch The Legacy
+        $legacies = Legacy::orderBy("id","desc")->whereNull('deleted_at')->get();
+
+        // === Fetch The Strength
+        $strengths = Strength::orderBy("id","desc")->whereNull('deleted_at')->get();
+
+        // ==== Fetch Our Logo
+        $ourLogos = OurLogo::orderBy("id","desc")->whereNull('deleted_at')->get();
+
+        return view("frontend.about.who-we-are", [
+            'journeys' => $journeys,
+            'members' => $members,
+            'progressDetails' => $progressDetails,
+            'legacies' => $legacies,
+            'strengths' => $strengths,
+            'ourLogos' => $ourLogos
+        ]);
     }
 }
