64bf07c7bd20ff3b8741c67d6ea71b26f32f1706
govind663 <whitdevil06@gmail.com>
2024-09-05 17:14:05 +0530

    Bhairaav | Version - 0.48

--- app/Http/Controllers/frontend/AssociatesController.php
+++ app/Http/Controllers/frontend/AssociatesController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\BackingPartner;
+use App\Models\Partner;
 use Illuminate\Http\Request;
 
 class AssociatesController extends Controller
 {
     public function associates(Request $request){
-        return view("frontend.about.associates");
+
+        $partners = Partner::orderBy("id","desc")->whereNull('deleted_at')->get();
+        $backingPartners = BackingPartner::orderBy("id","desc")->whereNull('deleted_at')->get();
+        return view("frontend.about.associates", ['backingPartners' => $backingPartners, 'partners' => $partners]);
     }
 }
