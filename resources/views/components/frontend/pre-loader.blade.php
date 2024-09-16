fe43af1d5818bb2b02719195c245b89bc967d784
govind663 <whitdevil06@gmail.com>
2024-09-16 15:11:14 +0530

    Bhairaav | Version - 0.106

--- resources/views/components/frontend/pre-loader.blade.php
+++ resources/views/components/frontend/pre-loader.blade.php
@@ -1,3 +1,5 @@
 <div class="cs_preloader cs_accent_color cs_primary_font">
-    <div class="cs_preloader_in">B</div>
+    <div class="cs_preloader_in">
+        <img src="{{ asset('frontend/assets/img/about/Bhairaav-black.png') }}"/>
+    </div>
 </div>
