01dea2e698918d4bfc95f54fcbd2b12adf9600c0
govind663 <whitdevil06@gmail.com>
2024-09-20 13:08:06 +0530

    Bhairaav | Version - 0.122

--- resources/views/backend/admin-dashboard.blade.php
+++ resources/views/backend/admin-dashboard.blade.php
@@ -29,9 +29,9 @@ Bhairaav | Home
         </div>
     </div>
 
-    {{-- <div class="title pb-20">
-        <h2 class="h3 mb-0">Hospital Overview</h2>
-    </div> --}}
+    <div class="title pb-20">
+        <h2 class="h3 mb-0">Project Overview</h2>
+    </div>
 
     <div class="row pb-10">
         <div class="col-xl-3 col-lg-3 col-md-6 mb-20">
